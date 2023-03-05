006cb5f0a5ddc35488e2aeba6f8a328523927120
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-03-05 18:09:12 +0530

    Create Quest2.c

---
+++ PROGRAMS/OS4/Quest2.c
@@ -0,0 +1,38 @@
+// Write a program that creates a single thread that generates random numbers between 1 and 100. The
+// thread should run for a specified number of iterations, printing each generated number to the screen.
+// After the specified number of iterations, the thread should stop and the main program should print the
+// sum of all the generated numbers.Also record and display time taken by each thread
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+#define NUM_ITERATIONS 10
+
+void* generate_random_numbers(void* arg);
+
+int main() {
+    pthread_t thread;
+    srand(time(NULL));
+    int sum = 0;
+    clock_t start_time, end_time;
+    start_time = clock();
+    pthread_create(&thread, NULL, generate_random_numbers, (void*)&sum);
+    pthread_join(thread, NULL);
+    end_time = clock();
+    double time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Total sum: %d\n", sum);
+    printf("Time taken: %f seconds\n", time_taken);
+    return 0;
+}
+
+void* generate_random_numbers(void* arg) {
+    int* sum = (int*)arg;
+    for (int i = 0; i < NUM_ITERATIONS; i++) {
+        int rand_num = rand() % 100 + 1;
+        printf("Random number %d: %d\n", i+1, rand_num);
+        *sum += rand_num;
+    }
+    pthread_exit(NULL);
+}
